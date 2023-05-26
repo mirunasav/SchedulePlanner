279cc02d4d5bfd15f7e13735957581d6a010d323
Savin Miruna <miruna.savin02@gmail.com>
2023-05-26 22:53:07 +0300

    Show new task permutation on the screen

--- SchedulePlannerClient/src/main/java/org/example/GUI/GenerateScheduleButtonListener.java
+++ SchedulePlannerClient/src/main/java/org/example/GUI/GenerateScheduleButtonListener.java
@@ -1,6 +1,8 @@
 package org.example.GUI;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.web.client.RestClientException;
@@ -9,6 +11,8 @@ import org.springframework.web.client.RestTemplate;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -57,8 +61,12 @@ public class GenerateScheduleButtonListener implements ActionListener {
 
             //access the response body
             String responseBody = response.getBody();
+            //write it in a json so I can analyze it
+            objectMapper.writeValue(new File("output.json"), responseBody);
 
-        } catch (RestClientException ex) {
+            //show the new list on the screen
+            this.processResponseBody(responseBody);
+        } catch (RestClientException | IOException ex) {
             ex.printStackTrace();
         }
     }
@@ -82,7 +90,7 @@ public class GenerateScheduleButtonListener implements ActionListener {
             durationHours = Integer.parseInt(hoursSubstring.substring(1, 2));
         else
             durationHours = Integer.parseInt(hoursSubstring);
-        int durationHoursToMinutes = Integer.parseInt(hoursSubstring) * 60;
+        int durationHoursToMinutes = durationHours * 60;
 
         // Extract the duration minutes
         int durationMinutesStartIndex = taskDetails.indexOf("minutes") - 3; // Find the index before the "minutes" text
@@ -121,5 +129,43 @@ public class GenerateScheduleButtonListener implements ActionListener {
 
         // Create and return the Task object
         return new Task(taskName, totalDurationTime, startTime, endTime);
+    }
+
+    private void processResponseBody(String responseBody) {
+        //extract numbers from response body
+        List<Integer> listOfNumbers = parseResponseBody(responseBody);
+
+        //create new default list model
+        DefaultListModel<String> newList = new DefaultListModel<>();
+
+        //populate the list with tasks
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            int index = listOfNumbers.get(i);
+            newList.add(i, taskListModel.get(index));
+        }
+        taskListModel.clear();
+        for (int i = 0; i < newList.getSize(); i++) {
+            taskListModel.addElement(newList.getElementAt(i));
+        }
+    }
+
+    private List<Integer> parseResponseBody(String responseBody) {
+        List<Integer> listOfNumbers = new ArrayList<>();
+        //response body  will look like this : [1,2,3,...n]
+
+        //remove parantheses
+        responseBody = responseBody.substring(1, responseBody.length() - 1);
+
+        if (responseBody.length() == 1) {//o sg cifra
+            listOfNumbers.add(Integer.parseInt(responseBody));
+            return listOfNumbers;
+        }
+
+        //split
+        String[] numbers = responseBody.split(",");
+        //convert to int
+        for (String number : numbers)
+            listOfNumbers.add(Integer.parseInt(number));
+        return listOfNumbers;
     }
 }
